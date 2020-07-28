2d6df34c69c52b84242dcae2e48a5bccc546b2fd
balajeyapalan <62101124+balajeyapalan@users.noreply.github.com>
2020-07-28 07:24:28 +0530

    Update Assignment_1_FindNumbersDivisibleFormat.py
    Updated the changes to print

--- Assignment_1_FindNumbersDivisibleFormat.py
+++ Assignment_1_FindNumbersDivisibleFormat.py
@@ -4,8 +4,9 @@ end_number = 3200
 result = " "
 while(start_number <= end_number):
     if(start_number % 7 == 0 ) and (start_number % 5 != 0 ):
-        result = result+","+str(start_number)
+        print(start_number,end=" ")
+        #result = result+","+str(start_number)
     start_number = start_number + 1
 
-print("Numbers are divisible by 7 and not multiple of 5 :\n " , result[2:len(result)])
+#print("Numbers are divisible by 7 and not multiple of 5 :\n " , result[2:len(result)])
 
