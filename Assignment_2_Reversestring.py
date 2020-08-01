bade87f004683ea129bc5285161e69c252e7e8fa
balajeyapalan <62101124+balajeyapalan@users.noreply.github.com>
2020-08-01 16:44:10 +0530

    Update Assignment_2_Reversestring.py
    Changes updated

--- Assignment_2_Reversestring.py
+++ Assignment_2_Reversestring.py
@@ -8,9 +8,10 @@ last_name = input("Please enter last name: ")
 fullname = first_name + " " + last_name
 count = len(fullname)
 reversed_string = ""
+'''
 while (count != 0):
     reversed_string = reversed_string + fullname[count - 1]
     # print(b[count-1])
     count = count - 1
-
-print("Reversed string is : ", reversed_string)
+'''
+print("Reversed string is : ", fullname[::-1])
